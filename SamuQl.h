a77e6197cca49a82bfd0636308007625b73e0ea4
Norbert Bátfai <batfai.norbert@inf.unideb.hu>
2016-04-17 20:57:16 +0200

    // -1   //TuringMachine<5> tm ( 9, 0, 9, 3, 20, 9, 1,  7, 29, 8, 12, 5, 24, 2, 5, 1, 11, 4, 17);

--- SamuQl.h
+++ SamuQl.h
@@ -1300,8 +1300,8 @@ private:
     double min_reward {-3.1};
     */
 
-    double max_reward {10.00};
-    double min_reward {-1000.00};
+    double max_reward {1.00};
+    double min_reward {-100000.00};
 
 #ifdef PLACE_VALUE
     double prev_image [10*3];
